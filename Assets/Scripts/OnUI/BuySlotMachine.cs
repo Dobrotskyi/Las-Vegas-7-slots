dad37142bae123644f3c66f7ec4a224309aa68ed
Valeriy <dobrotskiyvd@gmail.com>
2024-01-24 12:49:01 +0200

    UI Fix

--- Assets/Scripts/OnUI/BuySlotMachine.cs
+++ Assets/Scripts/OnUI/BuySlotMachine.cs
@@ -11,8 +11,16 @@ public class BuySlotMachine : MonoBehaviour
     [SerializeField] private GameObject _toggleGO;
     [SerializeField] private Button _button;
 
+    public void Buy()
+    {
+        PlayerInfoHolder.BuySlotMachine(_type);
+        OnEnable();
+    }
+
     private void Awake()
     {
+        PlayerPrefs.DeleteKey(_type.ToString());
+
         _price = PlayerInfoHolder.PriceListMachines[_type];
         _priceField.text = _price.ToString();
         PlayerInfoHolder.CasinoMoneyUpdated += OnEnable;
